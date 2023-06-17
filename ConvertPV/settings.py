530416e0b6e2ed8f7297a0121e6fad1e406d6333
Kodeleaf <kodeleafsoftwarepvtltd@gmail.com>
2023-06-17 22:51:33 +0530

    done vercel settings

--- ConvertPV/settings.py
+++ ConvertPV/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-^es4o9stea*r2hg(g@3e(va_a4r7qi*#tnvpn$ko9)5-!94-j^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_DIRS = [
     STATIC_DIR
 ]
