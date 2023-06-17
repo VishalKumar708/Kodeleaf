878a3470a56ffea691a17ea667969309e29fef6f
Kodeleaf <kodeleafsoftwarepvtltd@gmail.com>
2023-06-17 23:14:59 +0530

    error correct

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
-python 3.11.3 -m pip install -r requirements.txt
-python 3.11.3 manage.py collectstatic --noinput --clear
+python 3.11 -m pip install -r requirements.txt
+python 3.11 manage.py collectstatic --noinput --clear
 echo  "BUILD END"
